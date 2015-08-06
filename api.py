252e1754e0883417462f5b5dee5da74ca116697c
Vinicius Goulart <goulart.vs@gmail.com>
2015-08-06 14:33:32 -0300

    Adiciona funcionalidade básica da API

--- api.py
+++ api.py
@@ -1,9 +1,22 @@
-from flask import Flask
+from flask import Flask, request, render_template
+from flask_restful import reqparse, Api, Resource
+import subprocess
+
 app = Flask(__name__)
+api = Api(app)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template("index.htm")
+
+class Result(Resource):
+    def post(self):
+        p = subprocess.Popen('Rscript test.R', stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = p.communicate()
+        return {"result": out}, 201
+
+api.add_resource(Result, '/result')
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
