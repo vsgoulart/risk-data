d9707678270d22287dd8124ee730b33f6474d888
Vinicius Goulart <goulart.vs@gmail.com>
2015-09-04 13:55:56 -0300

    Adiciona a funcionalidade de carregar os campos do formulário via fields.json

--- api.py
+++ api.py
@@ -23,7 +23,13 @@ class Result(Resource):
         
         return {"data": json.dumps(json_data), "result": out.decode('latin-1').encode("utf-8"), "err": err.decode('latin-1').encode("utf-8")}, 201
 
+class Fields(Resource):
+    def get(self):
+        fieldsJSON = open('fields.json').read()
+        return {"data": fieldsJSON}, 200
+
 api.add_resource(Result, '/result')
+api.add_resource(Fields, '/fields')
 
 
 if __name__ == '__main__':
