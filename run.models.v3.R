3cb7da96cb242b7e6f2fd823539ca0f13a51a519
Vinicius Goulart <goulart.vs@gmail.com>
2015-11-06 20:59:38 -0200

    Corrige erro ao transformar os valores em um data frame no script R e altera tipo do campo da relação cintura-quadril

--- run.models.v3.R
+++ run.models.v3.R
@@ -11,12 +11,13 @@ open(stdin);
 json <- readLines(stdin,n=1);
 data = fromJSON(txt=json);
 
-server.data <- data.frame(c(a_ativfisica), c(a_bebexcessivo), c(as.numeric(a_cint)), c(a_escolar), c(a_gidade), c(a_has2), c(as.numeric(a_imc1)), c(a_imc2), 
-                          c(as.numeric(a_rcq)), c(a_sfcvdhard), c(diea133), c(hfda11), c(rcta8a));
+server.data <- data.frame(c(data$a_ativfisica), c(data$a_bebexcessivo), c(as.numeric(data$a_cint)), c(data$a_escolar), c(data$a_gidade),
+                          c(data$a_has2), c(as.numeric(data$a_imc1)), c(data$a_imc2), c(as.numeric(data$a_rcq)), c(data$a_sfcvdhard),
+                          c(data$diea133), c(data$hfda11), c(data$rcta8));
 colnames(server.data) <- c("a_ativfisica", "a_bebexcessivo", "a_cint", "a_escolar", "a_gidade", "a_has2", "a_imc1", "a_imc2", "a_rcq", "a_sfcvdhard", 
                            "diea133", "hfda11", "rcta8");
 
-#save(server.data, file="testData");
+save(server.data, file="testData");
 
 pred <- predict.web.glm(server.data,  DEBUG=FALSE);
 
